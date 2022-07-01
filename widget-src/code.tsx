ff2123bf8501d708aaac192a247e539f1e6592d5
Mirjam Banzhaf <mirjam.banzhaf@zendesk.com>
2022-07-01 11:58:10 +0200

    Added User name

--- widget-src/code.tsx
+++ widget-src/code.tsx
@@ -1,9 +1,12 @@
 const { widget } = figma;
-const { AutoLayout, Ellipse, Frame, Image, Rectangle, SVG, Text, useSyncedState, usePropertyMenu, useWidgetId, waitForTask} = widget;
+const { AutoLayout, Ellipse, Frame, Image, Rectangle, SVG, Text, useSyncedState, usePropertyMenu, useWidgetId, waitForTask, useEffect} = widget;
 
 function Widget() {
   const[checked, setChecked] = useSyncedState("checked", false)
   const[question, setQuestion] = useSyncedState("question", "Yes or no?")
+  const [showName, setShowName] = useSyncedState<boolean>('showName', true)
+  const [name, setName] = useSyncedState<string>('name', "")
+  const [photoUrl, setPhotoUrl] = useSyncedState<string | null>('photoUrl', null)
 
   usePropertyMenu(
     [
@@ -29,6 +32,7 @@ function Widget() {
     },
   )
 
+
   async function record(checked: boolean) {
     console.log("record: " + checked)
     waitForTask(new Promise(resolve => {
@@ -43,74 +47,73 @@ function Widget() {
   }
 
   return (
+    
     <Frame
       name="Checkbox"
       cornerRadius={4}
       overflow="visible"
       width={16}
       height={20}
-      onClick={async ()=>{
-        await record(!checked)
-        setChecked(!checked)
-      }}
+      onClick={async () => {
+        await record(!checked);
+        setChecked(!checked);
+      } }
     >
-      <Rectangle
-        hidden={!checked}
-        name="Container"
-        x={{
-          type: "center",
-          offset: 0,
-        }}
-        y={{
-          type: "center",
-          offset: 0,
-        }}
-        fill="#1F73B7"
-        cornerRadius={4}
-        strokeWidth={0}
-        strokeAlign="center"
-        width={16}
-        height={16}
-        hoverStyle={{
-          fill:"#144A75"
-        }}
-      />
-      <SVG
-        hidden={!checked}
-        name="Check"
-        x={{
-          type: "center",
-          offset: 0,
-        }}
-        y={{
-          type: "center",
-          offset: 0,
-        }}
-        height={6}
-        width={8}
-        src="<svg width='8' height='6' viewBox='0 0 8 6' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M3 3.58579L6.29289 0.292893C6.68342 -0.0976311 7.31658 -0.0976311 7.70711 0.292893C8.09763 0.683418 8.09763 1.31658 7.70711 1.70711L3.70711 5.70711C3.31658 6.09763 2.68342 6.09763 2.29289 5.70711L0.292893 3.70711C-0.0976311 3.31658 -0.0976311 2.68342 0.292893 2.29289C0.683418 1.90237 1.31658 1.90237 1.70711 2.29289L3 3.58579Z' fill='white'/></svg>"
-      />
-      <Rectangle
-        name="Container uc"
-        hidden={checked}
-        x={{
-          type: "center",
-          offset: 0,
-        }}
-        y={{
-          type: "center",
-          offset: 0,
-        }}
-        fill="#FFF"
-        stroke="#D8DCDE"
-        cornerRadius={4}
-        width={16}
-        height={16}
-        hoverStyle={{
-          fill:"#DDEAF4",
-          stroke:"#1F73B7"}}
-      />
-    </Frame>
+        <Rectangle
+          hidden={!checked}
+          name="Container"
+          x={{
+            type: "center",
+            offset: 0,
+          }}
+          y={{
+            type: "center",
+            offset: 0,
+          }}
+          fill="#1F73B7"
+          cornerRadius={4}
+          strokeWidth={0}
+          strokeAlign="center"
+          width={16}
+          height={16}
+          hoverStyle={{
+            fill: "#144A75"
+          }} />
+        <SVG
+          hidden={!checked}
+          name="Check"
+          x={{
+            type: "center",
+            offset: 0,
+          }}
+          y={{
+            type: "center",
+            offset: 0,
+          }}
+          height={6}
+          width={8}
+          src="<svg width='8' height='6' viewBox='0 0 8 6' fill='none' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' clip-rule='evenodd' d='M3 3.58579L6.29289 0.292893C6.68342 -0.0976311 7.31658 -0.0976311 7.70711 0.292893C8.09763 0.683418 8.09763 1.31658 7.70711 1.70711L3.70711 5.70711C3.31658 6.09763 2.68342 6.09763 2.29289 5.70711L0.292893 3.70711C-0.0976311 3.31658 -0.0976311 2.68342 0.292893 2.29289C0.683418 1.90237 1.31658 1.90237 1.70711 2.29289L3 3.58579Z' fill='white'/></svg>" />
+        <Rectangle
+          name="Container uc"
+          hidden={checked}
+          x={{
+            type: "center",
+            offset: 0,
+          }}
+          y={{
+            type: "center",
+            offset: 0,
+          }}
+          fill="#FFF"
+          stroke="#D8DCDE"
+          cornerRadius={4}
+          width={16}
+          height={16}
+          hoverStyle={{
+            fill: "#DDEAF4",
+            stroke: "#1F73B7"
+          }} />
+      </Frame>
     // <AutoLayout
     //   direction="horizontal"
     //   horizontalAlignItems="center"
@@ -141,3 +144,4 @@ function Widget() {
   );
 }
 widget.register(Widget);
+
